ef21ca9d8fea96ad7596589eb34b292c0194a562
Apurva Patode <patodeapurva01@gmail.com>
2023-04-21 11:38:36 +0530

    ethersjs example

---
+++ account.js
@@ -0,0 +1,14 @@
+
+const { ethers } = require("ethers");
+
+const INFURA_ID = ''
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+
+const address = '${account_address}'
+
+const main = async () => {
+    const balance = await provider.getBalance(address)
+    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
+}
+
+main()
